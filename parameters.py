14e3577e084994d5848c05a9dbcd9fc33f5185d1
magaudioso <magaudioso@gmail.com>
2021-06-04 22:05:22 -0400

    First Commit-script = topic + location = csv

---
+++ parameters.py
@@ -0,0 +1,9 @@
+search_query = 'site:linkedin.com/in/ AND "python developer" AND "New York"'
+
+
+# file were scraped profile information will be stored  
+file_name = 'results_file.csv'
+
+# login credentials
+linkedin_username = 'linkedin_email'
+linkedin_password = 'linkedin_password'
